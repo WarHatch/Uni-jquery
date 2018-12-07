77775049a1fd0d4cfa843b03dcff3daabe5f7e1b
WarHatch <satelistas@gmail.com>
2018-12-08 00:27:50 +0200

    HTML puslapio dinaminis turinio modifikavimas

--- jQuery.js
+++ jQuery.js
@@ -1,11 +1,11 @@
-function isNormalInteger(str) {
+function IsNormalInteger(str) {
     return /^([1-9]\d*)$/.test(str);
 }
 
-function validateAllInputBy(className, validationFunc) {
-    $(".validated-field").each(function (element) {
+function ValidateAllInputBy(className, validationFunc) {
+    $('.validated-field').each(function () {
         let inputElement = $(this).children(`input[class=${className}]`);
-        let errorElement = $(this).children(".error");
+        let errorElement = $(this).children('.error');
 
         inputElement.on('input', function () {
             validationFunc(inputElement.val()) ? errorElement.hide() : errorElement.show();
@@ -13,8 +13,17 @@ function validateAllInputBy(className, validationFunc) {
     });
 }
 
+function RemoveNegativeWords(str) {
+    return str.replace(/despicable/g, 'amazing');
+}
+
 jQuery(function () {
-    validateAllInputBy('mandatory', (value) => value); // empty strings are falsy
-    validateAllInputBy('integer', (value) => isNormalInteger(value));
-    validateAllInputBy('date', (value) => Date.parse(value));
+    ValidateAllInputBy('mandatory', (value) => value); // empty strings are falsy
+    ValidateAllInputBy('integer', (value) => IsNormalInteger(value));
+    ValidateAllInputBy('date', (value) => Date.parse(value));
+    
+    $("h1").text(RemoveNegativeWords($("h1").text()));
+    $("p").css("color", "grey");
+    $("li:nth-child(2)").remove();
+    $("#opinion-article").append('<p>There is nothing wrong here, continue with your business</p>');
 });
